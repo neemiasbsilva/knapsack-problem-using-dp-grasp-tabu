90f93b17522bd916c4cc9af00f64c13e2ed15eec
neemiasbsilva <neemiasbsilva@gmail.com>
2019-09-25 22:29:33 -0400

    Implementing graphic for the execution time for three methods

--- time-result-latex.py
+++ time-result-latex.py
@@ -1,10 +1,22 @@
 import pandas as pd
-
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 def main():
     df = pd.read_json("Output/execution-time.json").T
     df.drop(['Crescente', 'Decrescente'], inplace=True, axis=1)
-    df.to_latex("Output/table-latex.out")
+    # df.to_latex("Output/table-latex.out")
+    # df = df.T
+    df = df.rename(columns={"Eficiente": "Greedy",
+                         "GRASP": "GRASP",
+                         "Exato": "Dynamic Programming"})
+    # df = df.set_index('Instance')
+    # plt.figure()
+    df.rename_axis("Intances", inplace=True, axis="columns")
+    df.plot.bar(stacked=True, figsize=(7, 5))
+    plt.ylabel("Execution Time (min)")
+    plt.savefig("Graphics/execution-time.png")
+    # plt.show()
 
 if __name__ == "__main__":
     main()
