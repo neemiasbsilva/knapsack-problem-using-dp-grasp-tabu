1c5f5aaba528f18027e92b97a9e25339f882d1c8
neemiasbsilva <neemiasbsilva@gmail.com>
2019-10-01 18:15:36 -0400

    Implementing the graphic for tabu search table

--- Graphics/graphic-plots.py
+++ Graphics/graphic-plots.py
@@ -11,9 +11,11 @@ def main():
     fileGreedy = open("Output/greedy.out", "r")
     fileGrasp = open("Output/grasp.out", "r")
     fileDynamic = open("Output/dynamic.out", "r")
+    fileTabu = open("Output/tabu.out", "r")
     resultDynammic = []
     resultGreedy = []
     resultGrasp = []
+    resultTabu = []
     for line in fileGreedy:
         line = line.rstrip()
         i, numbers = re.findall("[0-9]+", line)
@@ -29,11 +31,16 @@ def main():
         i, numbers = re.findall("[0-9]+", line)
         resultDynammic.append(int(numbers))
 
+    for line in fileTabu:
+        line = line.rstrip()
+        i, numbers = re.findall("[0-9]+", line)
+        resultTabu.append(int(numbers))
+
     fileDynamic.close()
     fileGrasp.close()
     fileGreedy.close()
-
-    df = pd.DataFrame({"Greedy": resultGreedy, "Grasp": resultGrasp, "Dynamic": resultDynammic})
+    fileTabu.close()
+    df = pd.DataFrame({"Greedy": resultGreedy, "Grasp": resultGrasp, "Dynamic": resultDynammic, "Tabu": resultTabu})
     # df.plot.hist(bins=20, figsize=(10, 5), cmap="viridis")
     # sns.heatmap(df, yticklabels=False, cbar=False)
     # sns.countplot(x="Grasp", data=df)
@@ -43,7 +50,7 @@ def main():
     plt.savefig('Graphics/greedy-dynamic.png')
     df.plot.line()
     plt.savefig('Graphics/line-greedy-grasp-dynamic.png')
-    df.to_csv('Output/knapsack-result.csv')
+    df.to_csv('Output/knapsack-result-Tabu.csv')
     plt.show()
 
 if __name__ == "__main__":
